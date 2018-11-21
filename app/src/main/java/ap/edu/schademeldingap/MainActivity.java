162ae8795a58573090b5a2a16a907429328fd790
wa1id <wa1id@users.noreply.github.com>
2018-11-21 11:55:09 +0100

    Firebase geconfigureerd
    + hello world write test naar database

--- app/src/main/java/ap/edu/schademeldingap/MainActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/MainActivity.java
@@ -2,12 +2,38 @@ package ap.edu.schademeldingap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sendData;
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Name");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        sendData = findViewById(R.id.sendData);
+
+        sendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue("Hello world");
+                
+
+            }
+        });
+
     }
 }
