fd7de854dd414d035f2b3bfe3e77b49c54f521b6
wa1id <wa1id@users.noreply.github.com>
2018-11-28 16:25:38 +0100

    Inloggen en uitloggen

--- app/src/main/java/ap/edu/schademeldingap/HomeActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/HomeActivity.java
@@ -1,13 +1,36 @@
 package ap.edu.schademeldingap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private Button buttonSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonSignOut = findViewById(R.id.buttonSignOut);
+
+        //Tijdelijke sign out knop, moet ergens anders gezet worden
+        buttonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/ap/edu/schademeldingap/MainActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/MainActivity.java
@@ -1,23 +1,110 @@
 package ap.edu.schademeldingap;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Write a message to the database
+    private FirebaseAuth mAuth;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("Name");
+    private static final String TAG = "Aanmelden";
+
+    private Button buttonAanmelden;
+    private Button buttonRegistreren;
+    private EditText editEmail;
+    private EditText editWachtwoord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
 
+        buttonAanmelden = findViewById(R.id.buttonAanmelden);
+        buttonRegistreren = findViewById(R.id.buttonNietGeregistreerd);
+        editEmail = findViewById(R.id.editEmail);
+        editWachtwoord = findViewById(R.id.editWachtwoord);
 
+        buttonAanmelden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(editEmail.getText().toString(), editWachtwoord.getText().toString());
+            }
+        });
+
+        buttonRegistreren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegistreerActivity.class));
+                finish();
+            }
+        });
     }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //todo: show progressDialog
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authenticatie mislukt.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Authenticatie mislukt.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        //todo: hide progressBar
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        return valid;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Controleer of er al een user is ingelogd en verander UI
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            finish(); //zorgt ervoor dat de gebruiker niet terug kan door back button
+        }
+    }
+
+
 }
