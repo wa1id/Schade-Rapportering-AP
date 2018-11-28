0db6a3f4d52213113e2f3e1bfb2c6fc30f1e4d55
Ibrakhim Abdurachmanov <bleach_fanart@hotmail.com>
2018-11-28 15:53:55 +0100

    meldingen kunnen toevoegen aan database en input controle voorzien

--- app/src/main/java/ap/edu/schademeldingap/NieuweMeldingActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/NieuweMeldingActivity.java
@@ -1,21 +1,41 @@
 package ap.edu.schademeldingap;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class NieuweMeldingActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("meldingen");
+
+    private Button buttonMeldenSchade;
+    private EditText vrijeInvoer;
+    private EditText beschrijvingSchade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nieuwe_melding);
 
-        Spinner spinnerCategorie = findViewById(R.id.spinnerCategorie);
+        buttonMeldenSchade = findViewById(R.id.buttonMeldenSchade);
+        vrijeInvoer = findViewById(R.id.editVrijeInvoer);
+        beschrijvingSchade = findViewById(R.id.editBeschrijving);
+
+        final Spinner spinnerCategorie = findViewById(R.id.spinnerCategorie);
         final Spinner spinnerVerdieping = findViewById(R.id.spinnerVerdieping);
         final Spinner spinnerLokaal = findViewById(R.id.spinnerLokaal);
 
@@ -69,29 +89,57 @@ public class NieuweMeldingActivity extends AppCompatActivity {
         });
 
 
+             buttonMeldenSchade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!validateForm()) {
+                    return;
+                }
+
+
+                DatabaseReference myMelding = myRef.push();
+                myMelding.child("campus").setValue("ELL");
+                myMelding.child("lokaal").setValue(spinnerLokaal.getSelectedItem().toString());
+                myMelding.child("lokaal vrij invoer").setValue(vrijeInvoer.getText().toString());
+                myMelding.child("categorie").setValue(spinnerCategorie.getSelectedItem().toString());
+                myMelding.child("beschrijving schade").setValue(beschrijvingSchade.getText().toString());
+                myMelding.child("gerepareerd").setValue(false);
 
 
 
+                //Popup geslaagd tonen en naar andere activity gaan
+                AlertDialog.Builder builder;
 
-     /*  if(spinnerVerdieping.getSelectedItem().toString() == "-1"){
-           spinnerLokaal.setAdapter(adapterLokaalVerdiepMin1);
-       }else if (spinnerVerdieping.getSelectedItem().toString() == "0"){
-           spinnerLokaal.setAdapter(adapterLokaalVerdiepGelijkvloer);
-       }else if (spinnerVerdieping.getSelectedItem().toString() == "1"){
-           spinnerLokaal.setAdapter(adapterLokaalVerdiep1);
-       }else if (spinnerVerdieping.getSelectedItem().toString() == "2"){
-           spinnerLokaal.setAdapter(adapterLokaalVerdiep2);
-       }else if (spinnerVerdieping.getSelectedItem().toString() == "3"){
-           spinnerLokaal.setAdapter(adapterLokaalVerdiep3);
-       }else{
-           spinnerLokaal.setAdapter(adapterLokaalVerdiep4);
-       }*/
+                builder = new AlertDialog.Builder(NieuweMeldingActivity.this, android.R.style.Theme_Material_Dialog_Alert);
 
-      // switch (spinnerVerdieping.getSelectedItem().toString()){
-        //   case "-1":
+                builder.setTitle("Geslaagd!")
+                        .setMessage("Uw melding werd succesvol gecreëerd.")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                startActivity(new Intent(NieuweMeldingActivity.this, HomeActivity.class));
+                                finish(); //zorgt ervoor dat de gebruiker niet terug kan door back button
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .show();
 
-       //}
+            }
+        });
 
 
     }
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String beschrijving = beschrijvingSchade.getText().toString();
+        if (TextUtils.isEmpty(beschrijving)) {
+            beschrijvingSchade.setError("Verplicht.");
+            valid = false;
+        } else {
+            beschrijvingSchade.setError(null);
+        }
+        return valid;
+    }
+
 }
