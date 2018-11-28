4aa08d2cfb44f118f8e7bd0ca3855172adad0dbb
wa1id <wa1id@users.noreply.github.com>
2018-11-28 12:22:44 +0100

    "naam" en "reparateur" wordt opgeslagen in db onder de juiste userID

--- app/src/main/java/ap/edu/schademeldingap/RegistreerActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/RegistreerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,15 +15,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistreerActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("users");
     private static final String TAG = "registratie";
 
     private Button buttonRegistreer;
     private EditText editUsername;
     private EditText editPassword;
+    private EditText editName;
+    private CheckBox checkReparateur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,8 @@ public class RegistreerActivity extends AppCompatActivity {
         buttonRegistreer = findViewById(R.id.buttonRegister);
         editUsername = findViewById(R.id.editEmail);
         editPassword = findViewById(R.id.editPassword);
+        editName = findViewById(R.id.editName);
+        checkReparateur = findViewById(R.id.checkReparateur);
 
         buttonRegistreer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +70,14 @@ public class RegistreerActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     Log.d(TAG, "createUserWithEmail:success");
+
                     FirebaseUser user  = mAuth.getCurrentUser();
+
+                    //Extra user informatie die opgeslagen moet worden
+                    DatabaseReference myRefUser = myRef.child(user.getUid());
+                    myRefUser.child("naam").setValue(editName.getText().toString());
+                    myRefUser.child("reparateur").setValue(checkReparateur.isChecked());
+
                     updateUI(user);
                 } else {
                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
