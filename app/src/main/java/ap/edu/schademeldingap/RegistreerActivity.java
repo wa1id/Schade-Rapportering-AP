57736e93c6eb80d1205960135eb6cd8c337497a4
wa1id <wa1id@users.noreply.github.com>
2018-11-28 11:35:28 +0100

    Gebruiker kan registreren door email en wachtwoord te kiezen

--- app/src/main/java/ap/edu/schademeldingap/RegistreerActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/RegistreerActivity.java
@@ -1,13 +1,83 @@
 package ap.edu.schademeldingap;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistreerActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private static final String TAG = "registratie";
+
+    private Button buttonRegistreer;
+    private EditText editUsername;
+    private EditText editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registreer);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonRegistreer = findViewById(R.id.buttonRegister);
+        editUsername = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+
+        buttonRegistreer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(editUsername.getText().toString(), editPassword.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) { //todo: form valideren, bv. email is required
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user  = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(RegistreerActivity.this, "Registratie mislukt", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    private boolean validateForm() {
+
+        return true;
+    }
+
+    private void updateUI(FirebaseUser user) { //todo: wordt opgeroepen wanneer er user doorverwezen moet worden naar de juiste activity na inloggen (home)
+
     }
 }
