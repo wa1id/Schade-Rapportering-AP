0ba61f9c985c1b2c570b3b2eda037f3c2b33185d
wa1id <wa1id@users.noreply.github.com>
2018-11-30 13:50:09 +0100

    Camera opent na klikken button

--- app/src/main/java/ap/edu/schademeldingap/NieuweMeldingActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/NieuweMeldingActivity.java
@@ -1,40 +1,56 @@
 package ap.edu.schademeldingap;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Switch;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class NieuweMeldingActivity extends AppCompatActivity {
 
-    //database connectie
+    //firebase stuff
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference().child("meldingen");
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReference().child("images");
 
     //variabelen
     private Button buttonMeldenSchade;
+    private Button buttonFoto;
     private EditText vrijeInvoer;
     private EditText beschrijvingSchade;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nieuwe_melding);
 
+        //Permissions
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 2);
+        }
+
         //variabelen linken aan de UI
         buttonMeldenSchade = findViewById(R.id.buttonMeldenSchade);
+        buttonFoto = findViewById(R.id.buttonFoto);
         vrijeInvoer = findViewById(R.id.editVrijeInvoer);
         beschrijvingSchade = findViewById(R.id.editBeschrijving);
         final Spinner spinnerCategorie = findViewById(R.id.spinnerCategorie);
@@ -42,13 +58,13 @@ public class NieuweMeldingActivity extends AppCompatActivity {
         final Spinner spinnerLokaal = findViewById(R.id.spinnerLokaal);
 
         ArrayAdapter<CharSequence> adapterCategorie = ArrayAdapter.createFromResource(this, R.array.categorien, android.R.layout.simple_spinner_item);
-        ArrayAdapter<CharSequence> adapterVerdieping = ArrayAdapter.createFromResource(this,R.array.verdieping, android.R.layout.simple_spinner_item);
-        final ArrayAdapter<CharSequence> adapterLokaalVerdiepMin1 = ArrayAdapter.createFromResource(this,R.array.lokaalVerdiepMin1, android.R.layout.simple_spinner_item);
-        final ArrayAdapter<CharSequence> adapterLokaalVerdiepGelijkvloer = ArrayAdapter.createFromResource(this,R.array.lokaalVerdiepGelijkVloer, android.R.layout.simple_spinner_item);
-        final ArrayAdapter<CharSequence> adapterLokaalVerdiep1 = ArrayAdapter.createFromResource(this,R.array.lokaalVerdiep1, android.R.layout.simple_spinner_item);
-        final ArrayAdapter<CharSequence> adapterLokaalVerdiep2 = ArrayAdapter.createFromResource(this,R.array.lokaalVerdiep2, android.R.layout.simple_spinner_item);
-        final ArrayAdapter<CharSequence> adapterLokaalVerdiep3 = ArrayAdapter.createFromResource(this,R.array.lokaalVerdiep3, android.R.layout.simple_spinner_item);
-        final ArrayAdapter<CharSequence> adapterLokaalVerdiep4 = ArrayAdapter.createFromResource(this,R.array.lokaalVerdiep4, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapterVerdieping = ArrayAdapter.createFromResource(this, R.array.verdieping, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterLokaalVerdiepMin1 = ArrayAdapter.createFromResource(this, R.array.lokaalVerdiepMin1, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterLokaalVerdiepGelijkvloer = ArrayAdapter.createFromResource(this, R.array.lokaalVerdiepGelijkVloer, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterLokaalVerdiep1 = ArrayAdapter.createFromResource(this, R.array.lokaalVerdiep1, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterLokaalVerdiep2 = ArrayAdapter.createFromResource(this, R.array.lokaalVerdiep2, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterLokaalVerdiep3 = ArrayAdapter.createFromResource(this, R.array.lokaalVerdiep3, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> adapterLokaalVerdiep4 = ArrayAdapter.createFromResource(this, R.array.lokaalVerdiep4, android.R.layout.simple_spinner_item);
 
 
         adapterCategorie.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -70,7 +86,7 @@ public class NieuweMeldingActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedItem = parent.getItemAtPosition(position).toString();
 
-                switch (selectedItem){
+                switch (selectedItem) {
                     case "-1":
                         spinnerLokaal.setAdapter(adapterLokaalVerdiepMin1);
                         break;
@@ -99,15 +115,21 @@ public class NieuweMeldingActivity extends AppCompatActivity {
             }
         });
 
+        //Button listerens
+        buttonFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
-             buttonMeldenSchade.setOnClickListener(new View.OnClickListener() {
+        buttonMeldenSchade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 if (!validateForm()) {
                     return;
                 }
-
 
                 DatabaseReference myMelding = myRef.push();
                 myMelding.child("campus").setValue("ELL");
@@ -116,7 +138,6 @@ public class NieuweMeldingActivity extends AppCompatActivity {
                 myMelding.child("categorie").setValue(spinnerCategorie.getSelectedItem().toString());
                 myMelding.child("beschrijving schade").setValue(beschrijvingSchade.getText().toString());
                 myMelding.child("gerepareerd").setValue(false);
-
 
 
                 //Popup geslaagd tonen en naar andere activity gaan
@@ -137,9 +158,9 @@ public class NieuweMeldingActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
+
+
     private boolean validateForm() {
         boolean valid = true;
 
@@ -151,6 +172,13 @@ public class NieuweMeldingActivity extends AppCompatActivity {
             beschrijvingSchade.setError(null);
         }
         return valid;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
 }
