113099275fd3d6b523d0930d62809a9fc27ae68a
Ibrakhim Abdurachmanov <bleach_fanart@hotmail.com>
2018-12-01 14:45:47 +0100

    gefixt

--- app/src/main/java/ap/edu/schademeldingap/LijstHuidigeSchades.java
+++ app/src/main/java/ap/edu/schademeldingap/LijstHuidigeSchades.java
@@ -2,6 +2,7 @@ package ap.edu.schademeldingap;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -71,5 +72,8 @@ public class LijstHuidigeSchades extends AppCompatActivity {
 
             }
         });
+
+
+
     }
 }
