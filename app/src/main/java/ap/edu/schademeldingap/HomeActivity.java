e3e8f7abe8596a8c50a4ed27053f0a9b5045233d
wa1id <wa1id@users.noreply.github.com>
2018-12-01 16:05:28 +0100

    Ingelogde gebruiker Naam tonen op HomeActivity

--- app/src/main/java/ap/edu/schademeldingap/HomeActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/HomeActivity.java
@@ -1,20 +1,32 @@
 package ap.edu.schademeldingap;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference refUsers;
 
     private Button buttonSignOut;
     private Button buttonSchadeMelden;
     private Button buttonSchadeZoeken;
+    private TextView textWelkom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +34,15 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         mAuth = FirebaseAuth.getInstance();
+        refUsers = database.getReference().child("users");
 
         buttonSignOut = findViewById(R.id.buttonSignOut);
         buttonSchadeMelden = findViewById(R.id.buttonSchadeMelden);
         buttonSchadeZoeken = findViewById(R.id.buttonSchadezoeken);
+        textWelkom = findViewById(R.id.textWelkom);
+
+        //Titel van view veranderen naar naam van huidige user
+        setNaam(mAuth.getCurrentUser(), textWelkom);
 
         //Tijdelijke sign out knop, moet ergens anders gezet worden
         buttonSignOut.setOnClickListener(new View.OnClickListener() {
@@ -49,5 +66,20 @@ public class HomeActivity extends AppCompatActivity {
                startActivity(new Intent(HomeActivity.this, LijstHuidigeSchades.class));
            }
        });
+    }
+
+    private void setNaam(FirebaseUser user, final TextView textView) {
+
+        refUsers.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                textView.setText(textView.getText() + " " + dataSnapshot.child("naam").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("home", "failed addListenerForSingleValueEvent");
+            }
+        });
     }
 }
