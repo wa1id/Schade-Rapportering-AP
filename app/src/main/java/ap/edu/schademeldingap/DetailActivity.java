6e9988d9e86c831b8e24442203b3cc28af9e44e4
wa1id <wa1id@users.noreply.github.com>
2018-12-05 15:19:36 +0100

    Tekst wijzigingen

--- app/src/main/java/ap/edu/schademeldingap/DetailActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/DetailActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -11,6 +12,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.HashMap;
 
@@ -18,6 +21,7 @@ public class DetailActivity extends AppCompatActivity {
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference meldingRef = database.getReference().child("meldingen");
+    private StorageReference storageRef = FirebaseStorage.getInstance().getReference();
 
     private String id;
     private TextView textLokaal;
@@ -26,6 +30,7 @@ public class DetailActivity extends AppCompatActivity {
     private TextView textDatum;
     private TextView textBeschrijving;
     private TextView textGerepareerd;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +44,23 @@ public class DetailActivity extends AppCompatActivity {
         textDatum = findViewById(R.id.textDatum);
         textBeschrijving = findViewById(R.id.textBeschrijving2);
         textGerepareerd = findViewById(R.id.textGerepareerd);
+        imageView = findViewById(R.id.imageSchade);
 
         meldingRef.child(id).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                textLokaal.setText(dataSnapshot.child("lokaal").getValue().toString());
+                textLokaal.setText("Lokaal" + dataSnapshot.child("lokaal").getValue().toString());
                 textLokaalExtra.setText(dataSnapshot.child("lokaal vrije invoor").getValue().toString());
-                textCategorie.setText(dataSnapshot.child("categorie").getValue().toString());
-                textDatum.setText(dataSnapshot.child("datum").getValue().toString());
+                textCategorie.setText("Categorie" + dataSnapshot.child("categorie").getValue().toString());
+                textDatum.setText("Datum" + dataSnapshot.child("datum").getValue().toString());
                 textBeschrijving.setText(dataSnapshot.child("beschrijving schade").getValue().toString());
                 if (dataSnapshot.child("gerepareerd").getValue().toString().equals(false)) {
-                    textGerepareerd.setText("Nee");
+                    textGerepareerd.setText("Gerepareerd: Nee");
                 } else {
-                    textGerepareerd.setText("Ja");
+                    textGerepareerd.setText("Gerepareerd: Ja");
                 }
+
+
             }
 
             @Override
