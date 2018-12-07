7917376df96a82b511ccb587bb45b380d81cf651
Ibrakhim Abdurachmanov <bleach_fanart@hotmail.com>
2018-12-07 13:13:07 +0100

    removed unused imports

--- app/src/main/java/ap/edu/schademeldingap/LijstHuidigeSchades.java
+++ app/src/main/java/ap/edu/schademeldingap/LijstHuidigeSchades.java
@@ -2,23 +2,16 @@ package ap.edu.schademeldingap;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
+
 import android.view.WindowManager;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.Spinner;
 
 import com.google.firebase.database.ChildEventListener;
@@ -26,7 +19,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
