e769d841073527c281ccce8c490ac483bb110dcd
Ibrakhim Abdurachmanov <bleach_fanart@hotmail.com>
2018-12-08 17:54:29 +0100

    activity en layout van archive toegevoegd

---
+++ app/src/main/java/ap/edu/schademeldingap/activities/ArchiveActivity.java
@@ -0,0 +1,15 @@
+package ap.edu.schademeldingap.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ap.edu.schademeldingap.R;
+
+public class ArchiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archive);
+    }
+}
