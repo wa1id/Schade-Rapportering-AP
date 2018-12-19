8fdd6bd2daeef33969d2ee83cf7c7cbd17f2957d
wa1id <wa1id@users.noreply.github.com>
2018-12-19 16:59:14 +0100

    fix password reset crash

--- app/src/main/java/ap/edu/schademeldingap/activities/WachtwoordVergetenActivity.java
+++ app/src/main/java/ap/edu/schademeldingap/activities/WachtwoordVergetenActivity.java
@@ -2,9 +2,11 @@ package ap.edu.schademeldingap.activities;
 
 import android.support.annotation.NonNull;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -13,6 +15,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import ap.edu.schademeldingap.R;
 
 public class WachtwoordVergetenActivity extends AbstractActivity {
+
+    private EditText mEditMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +27,43 @@ public class WachtwoordVergetenActivity extends AbstractActivity {
 
         final FirebaseAuth auth = FirebaseAuth.getInstance();
 
-        final EditText editEmail = findViewById(R.id.editEmailHerstellen);
+        mEditMail = findViewById(R.id.editEmailHerstellen);
         Button buttonReset = findViewById(R.id.buttonWachtwoordHestellen);
-
-        final String email = editEmail.getText().toString();
 
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                if (!validateForm()) {
+                    return;
+                }
+
+                auth.sendPasswordResetEmail(mEditMail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        showDialogInfoToActivity(WachtwoordVergetenActivity.this, MainActivity.class,
-                                getString(R.string.geslaagd),
-                                getString(R.string.wachtwoord_reset_email_verzonden));
+                        if (task.isSuccessful()) {
+                            showDialogInfoToActivity(WachtwoordVergetenActivity.this, MainActivity.class,
+                                    getString(R.string.geslaagd),
+                                    getString(R.string.wachtwoord_reset_email_verzonden));
+                        } else {
+                            mEditMail.setError(getString(R.string.ongeldige_email));
+                        }
                     }
                 });
             }
         });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        //check for empty email
+        String email = mEditMail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEditMail.setError(getString(R.string.verplicht));
+            valid = false;
+        } else {
+            mEditMail.setError(null);
+        }
+        return valid;
     }
 }
