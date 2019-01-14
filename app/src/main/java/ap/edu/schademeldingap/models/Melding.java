c9a2e0c0db5f1d343c768b5102c22f54de22db50
Ibrakhim Abdurachmanov <bleach_fanart@hotmail.com>
2019-01-14 19:13:12 +0100

    saving new keys in database

--- app/src/main/java/ap/edu/schademeldingap/models/Melding.java
+++ app/src/main/java/ap/edu/schademeldingap/models/Melding.java
@@ -17,6 +17,10 @@ public class Melding {
     private String datum;
     private String id;
     private String token; //used for notifications
+    private String reparatieDatum;
+    private String reparatieUitvoerder;
+    private String extraNotities;
+    private String naamReparatieUitvoerder;
     private boolean gerepareerd;
 
     public Melding() {
@@ -34,62 +38,77 @@ public class Melding {
         Date date = new Date();
         this.datum = new SimpleDateFormat("dd/MM/yyyy").format(date);
         this.gerepareerd = false;
-        this.id = "";
-        this.token = "";
+    }
+    public String getReparatieDatum() {
+        return reparatieDatum;
     }
 
+    public void setReparatieDatum(String reparatieDatum) {
+        this.reparatieDatum = reparatieDatum;
+    }
+
+    public String getReparatieUitvoerder() {
+        return reparatieUitvoerder;
+    }
+
+    public void setReparatieUitvoerder(String reparatieUitvoerder) {
+        this.reparatieUitvoerder = reparatieUitvoerder;
+    }
+
+    public String getExtraNotities() {
+        return extraNotities;
+    }
+
+    public void setExtraNotities(String extraNotities) {
+        this.extraNotities = extraNotities;
+    }
+
+    public String getNaamReparatieUitvoerder() {
+        return naamReparatieUitvoerder;
+    }
+
+    public void setNaamReparatieUitvoerder(String naamReparatieUitvoerder) {
+        this.naamReparatieUitvoerder = naamReparatieUitvoerder;
+    }
     public String getName() {
         return name;
     }
-
     public String getLokaal() {
         return lokaal;
     }
-
     public String getVrijeInvoerLokaal() {
         return vrijeInvoerLokaal;
     }
-
     public String getCategorie() {
         return categorie;
     }
-
     public String getBeschrijvingSchade() {
         return beschrijvingSchade;
     }
-
     public String getDatum() {
         return datum;
     }
-
     public boolean isGerepareerd() {
         return gerepareerd;
     }
-
     public String getVerdieping() {
         return verdieping;
     }
-
     public String getCampus() {
         return campus;
     }
-
     public String getId() {
         return id;
     }
-
     public void setGerepareerd(boolean gerepareerd) {
         this.gerepareerd = gerepareerd;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getToken() {
         return token;
     }
-
     public void setToken(String token) {
         this.token = token;
     }
